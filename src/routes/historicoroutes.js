d7b3c9f4f019f51ae7d4a65ab63ab9159f2cf734
Jhonatan D. Silva <Jhonatandomingues1122@outlook.com>
2025-03-18 20:14:11 -0300

    Delete src directory

--- src/routes/historicoroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const estoqueController = require("../controllers/estoqueController");
-
-// Rota para buscar o histórico de alterações no estoque
-router.get("/", estoqueController.getHistoricoEstoque);
-
-module.exports = router;
