ff1d30646d9a6bc1415fbc95059ca3643644e2a7
jhonatan1973 <jhonatandomingues1122@outlook.com>
2025-03-18 20:58:41 -0300

    Primeiro commit

---
+++ assets/js/imobilizado-component/retirar-component.js
@@ -0,0 +1,35 @@
+function retirarImobilizado() {
+  const codigo = document.getElementById("codigo-imobilizado")?.value;
+  const quantidade = parseInt(
+    document.getElementById("quantidade-imobilizado")?.value
+  );
+
+  if (!codigo || isNaN(quantidade) || quantidade <= 0) {
+    alert("Informe um código válido e uma quantidade maior que zero.");
+    return;
+  }
+
+  // Encode the code to ensure it's valid in the URL
+  const codigoCodificado = encodeURIComponent(codigo);
+
+  fetch(`http://localhost:3000/imobilizados/${codigoCodificado}/retirar`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ quantidade }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        alert(data.error);
+      } else {
+        alert("Quantidade retirada com sucesso!");
+        fecharModAlterarImob(); // Fecha o modal após a operação
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao retirar quantidade:", error);
+      alert("Erro ao retirar quantidade.");
+    });
+}
