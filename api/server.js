f8d5235a20ae12ca3738acb9c4c76333ff35d027
jhonatan1973 <jhonatandomingues1122@outlook.com>
2025-04-07 09:26:00 -0300

    atualizando a conecção com banco de dados

--- api/server.js
+++ api/server.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const mysql = require("mysql2");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
-
+require("dotenv").config({ path: __dirname + "/.env" });
 const app = express();
 app.use(cors());
 app.use(express.json());
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_PORT);
-
 const db = mysql.createConnection({
-  host: "shuttle.proxy.rlwy.net",
-  user: "root",
-  password: "zIkWVucHmNXYsTGRtPvAJStlxxyvpTXt",
-  database: "railway",
-  port: 40063,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 db.connect(function (err) {
   if (err) {
